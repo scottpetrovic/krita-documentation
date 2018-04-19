12aa14f20e7a779719d28a1fd4948422e3b3decb
Scott Petrovic <spetrovic@swankmp.com>
2018-04-19 09:29:45 -0500

    get rid of a couple of warning that were happening because of image localization and epub exporting

--- conf.py
+++ conf.py
@@ -148,7 +148,7 @@ latex_documents = [
 
 locale_dirs = ['locale/']   # Where the PO files will be stored at
 gettext_compact = False     # optional.
-gettext_additional_targets = 'image' # allows images to be translatable
+gettext_additional_targets = ['image'] # allows images to be translatable
 
 
 
@@ -181,6 +181,7 @@ epub_title = project
 epub_author = author
 epub_publisher = author
 epub_copyright = copyright
+version = '1.0'
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
